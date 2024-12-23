e936bfbdc3a1ff27f9dacedd9045946efed4d095
Ahmad Irfan <ahmadirfan749@yahoo.com>
2024-12-23 15:15:55 +0500

    Import statements optimized

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,11 +7,6 @@ import LatestBlogs from '../components/LatestBlogs'
 import SavedBlogs from '../components/SavedBlogs'
 import LikedBlogs from '../components/LikedBlogs'
 import BottomNav from '../components/BottomNav'
-import ProfilePage from './ProfilePage'
-import BlogPostPage from './BlogPostPage'
-import CreateBlogPage from './CreateBlogPage'
-import SearchPage from './SearchPage'
-import SignInPage from './SignInPage'
 
 const HomePage = () => {
   return (
