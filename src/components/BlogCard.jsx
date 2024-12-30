4f5f1c9ce6c3801a2d572779d6bc39256d81e127
Ahmad Irfan <ahmadirfan749@yahoo.com>
2024-12-30 10:06:16 +0500

    Blog cards made dynamic - now fetch data from the database

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -5,10 +5,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart, faComment} from '@fortawesome/free-regular-svg-icons'
 import testImg from '../test-img.jpg'
 import UserInterest from './UserInterest'
-const BlogCard = () => {
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+const BlogCard = ({ title, image, content, dateCreated }) => {
 
     const heartIcon = <FontAwesomeIcon icon={faHeart} />
     const commentIcon = <FontAwesomeIcon icon={faComment} />
+
+    const formatDate = (dateString) => {
+        try {
+          return formatDistanceToNow(new Date(dateString), { addSuffix: true });
+        } catch (error) {
+          console.error("Invalid date value:", dateString, error);
+          return "Invalid date";
+        }
+      };
 
   return (
     <div className='flex flex-col p-4 my-4 mx-2 rounded-md bg-gray-100 border-2 border-gray-200'>
@@ -18,18 +29,18 @@ const BlogCard = () => {
                 <UserInterest />
 
                 <div className='my-2'>
-                    <span className='font-bold'>Generative AI taking everything by storm. Its so cool to see how its happening because were fucked to be very honest</span>
+                    <span className='font-bold'>{title}</span>
                 </div>
             </div>
             
 
             <div className='vsm:w-28 vsm:h-20 md:w-36 md:h-28 lg:w-44 shrink-0 w-full h-32'>
-                <img className='w-full h-full rounded-md' src={testImg}></img>
+                <img className='w-full h-full rounded-md border-2 border-gray-200' src={`http://localhost:5000/blogimages/${image}`}></img>
             </div>
         </div>
 
         <div className='flex justify-between py-2 w-full *:text-gray-400 md:mt-4'>
-                <span>1 hour ago</span>
+                <span>{formatDate(dateCreated)}</span>
                 <div className='*:mx-1'>
                     <button>{heartIcon}</button>
                     <span >10</span>
