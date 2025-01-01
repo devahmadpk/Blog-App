9e956a41e852f964ddea61734142f7202b38aa19
Ahmad Irfan <ahmadirfan749@yahoo.com>
2025-01-01 10:16:17 +0500

    Fetch from database functionality added

--- src/components/InterestBox.jsx
+++ src/components/InterestBox.jsx
@@ -1,11 +1,49 @@
 
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Interests from './Interests'
+import axios from 'axios';
 
 const InterestBox = () => {
+
+  const [interests, setInterests] = useState([]);
+  const [selectedInterests, setSelectedInterests] = useState([]);
+
+  useEffect(() => {
+
+    const fetchInterests = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/interests');
+        setInterests(response.data);
+      }
+      catch (err) {
+        console.error('Failed to fetch interests:', err);
+    }
+  }
+
+  fetchInterests();
+    
+  }, []);
+
+  const toggleInterest = (interest) => {
+    setSelectedInterests((prevSelected) => {
+      if(prevSelected.includes(interest)) {
+        return prevSelected.filter((item) => item !== interest);
+      }
+
+      return [...prevSelected, interest];
+    })
+  }
+
   return (
-    <div className='my-4 mx-2 '>
-        <span className='rounded-full bg-gray-200 text-black px-4 py-2'>Technology</span>
+    <div className='my-4 flex flex-wrap items-center '>
+      {
+        interests.length !== 0 && interests ? (
+          interests.map((interest) => (
+            <Interests key={interest.interest_id} interest={interest.name} onClick={() => toggleInterest(interest.name)} isSelected={selectedInterests.includes(interest.name)}/>
+        ))
+        ) : ( <p>No interests found </p>)
+      }
     </div>
   )
 }
