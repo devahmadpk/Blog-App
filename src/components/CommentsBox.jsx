5adb0cf54be233e4581d3037c9a01a6f7983b4e3
Ahmad Irfan <ahmadirfan749@yahoo.com>
2025-01-01 10:15:02 +0500

    Comments made dynamic by fetching data from database

--- src/components/CommentsBox.jsx
+++ src/components/CommentsBox.jsx
@@ -1,17 +1,17 @@
 
 
 import React from 'react'
-import testImg from '../test-img.jpg'
 
-const CommentsBox = () => {
+const CommentsBox = ({comment}) => {
   return (
+    
     <div className='p-4 bg-gray-200 my-4 rounded-md flex flex-col vsm:flex-row items-start vsm:items-center justify-center sm:justify-start md:w-4/5'>
         <div className='shrink-0'>
-            <img src={testImg} className='rounded-full w-12 h-12'></img>
+            <img src={`http://localhost:5000/uploads/${comment.profile_pic}`} className='rounded-full w-12 h-12'></img>
         </div>
         <div className='my-4 vsm:mx-8'>
-            <h3 className='font-bold'>Ahmad</h3>
-            <p className='mt-2'>This is my comment about your blog you bastard. Go kill yourself. I hope you die a slow painful death.</p>
+            <h3 className='font-bold'>{comment.username}</h3>
+            <p className='mt-2'>{comment.comment_text}</p>
         </div>
     </div>
   )
